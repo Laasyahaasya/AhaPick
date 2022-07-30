fe87ebf06bfee2776f9aac77117061b00c4322b6
Laasyahaasya <104257518+Laasyahaasya@users.noreply.github.com>
2022-07-30 11:02:31 +0530

    Jenkins Updated

--- src/charitha/FirstPrograminJava.java
+++ src/charitha/FirstPrograminJava.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeTest;
 public class FirstPrograminJava {
   @Test
   public void java() {
-	  System.out.println("Hello GitHub");
+	  System.out.println("Hello Jenkins");
   }
   @BeforeTest
   public void beforeTest() {
