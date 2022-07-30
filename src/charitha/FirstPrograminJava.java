593678f65aeca26a4a6a7b559e56cd92e839ac35
Laasyahaasya <104257518+Laasyahaasya@users.noreply.github.com>
2022-07-30 10:45:52 +0530

    First program class created

---
+++ src/charitha/FirstPrograminJava.java
@@ -0,0 +1,15 @@
+package charitha;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class FirstPrograminJava {
+  @Test
+  public void f() {
+	  System.out.println("Hello World");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
